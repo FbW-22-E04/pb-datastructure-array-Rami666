db19e07637b946ee4281690eb67a6668ca84bf12
Rami B <ramibasheer9@gmail.com>
2022-07-11 03:05:55 +0200

    submit changes

---
+++ solution.js
@@ -0,0 +1,11 @@
+//1
+const myArray = ['Rami', 'mohammad', 'Ahmad', 'Rabo', 'Rima']
+console.log(myArray);
+
+//2
+console.log(myArray[2]);
+const thirdElement = 'Ahmad'
+
+//3
+myArray.splice(3, 1, 'fourth element');
+console.log(myArray);
